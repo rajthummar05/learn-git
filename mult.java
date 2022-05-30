9d73ad4ef517d441db215dab218ee9df42bf648f
Raj Thummar <thummarraj051122@gmail.com>
2022-05-30 20:16:51 +0530

    added message in mult.java

---
+++ mult.java
@@ -0,0 +1,7 @@
+public class mult{
+    public static void main(String[]args){
+        int a = 5;
+        int b = 5;
+        System.out.println("The mult. of given number is =  "+(a*b));
+    }
+}
