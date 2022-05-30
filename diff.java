61c5648c025873e8265c2bafbe9b06060991b0da
Raj Thummar <thummarraj051122@gmail.com>
2022-05-30 20:41:03 +0530

    adding something in diff.java

---
+++ diff.java
@@ -0,0 +1,7 @@
+public class mult{
+    public static void main(String[]args){
+        int a = 5;
+        int b = 5;
+        System.out.println("The div. of given number is =  "+(a/b));
+    }
+}
