48d62181c0ff75eb701f2a710200b866630e23f7
Raj Thummar <thummarraj051122@gmail.com>
2022-05-30 20:08:16 +0530

    added message in sum.java

--- prac.java
+++ prac.java
@@ -2,6 +2,6 @@ public class prac{
     public static void main(String[]args){
         int a = 5;
         int b = 5;
-        System.out.println((a+b));
+        System.out.println("The sum of given number is =  "+(a+b));
     }
 }
